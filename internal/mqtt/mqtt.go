69494a7c4d14e4d994a08083794848bf0119af08
Dave O'Connor <git@mail.andvari.net>
2024-05-13 23:12:13 +0000

    Convert to goroutines.

--- internal/mqtt/mqtt.go
+++ internal/mqtt/mqtt.go
@@ -2,8 +2,11 @@ package mqtt
 
 import (
 	"log"
+	"strings"
+	"sync"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/gerrowadat/nut2mqtt/internal/upsc"
 )
 
 type mqttClient struct {
@@ -36,9 +39,12 @@ func (c *mqttClient) SetTopicBase(topic_base string) {
 	c.topic_base = topic_base
 }
 
-func (c *mqttClient) PublishMessage(topic string, content string) error {
-	log.Print("Publish: " + c.topic_base + topic + " => " + content)
-	pub_tok := c.c.Publish(c.topic_base+topic, 0, false, content)
+func (c *mqttClient) GetTopicBase() string {
+	return c.topic_base
+}
+
+func (c *mqttClient) PublishMessage(msg *MQTTUpdate) error {
+	pub_tok := c.c.Publish(c.topic_base+msg.topic, 0, false, msg.content)
 	pub_tok.Wait()
 
 	if pub_tok.Error() != nil {
@@ -49,4 +55,52 @@ func (c *mqttClient) PublishMessage(topic string, content string) error {
 
 func (c *mqttClient) Disconnect(code uint) {
 	c.c.Disconnect(code)
+}
+
+// A struct to describe an MQTT update to be made.
+type MQTTUpdate struct {
+	topic       string
+	content     string
+	old_content string
+}
+
+func (c *mqttClient) ChannelUpdates(ups_chan chan *upsc.UPSInfo, change_chan chan *MQTTUpdate, wg *sync.WaitGroup) {
+	defer wg.Done()
+	last_ups := map[string]*upsc.UPSInfo{}
+	for {
+		ups := <-ups_chan
+		old, present := last_ups[ups.Name()]
+		if present {
+			changed_vars := upsc.GetVarDiff(old, ups)
+			if len(changed_vars) > 0 {
+				for k, v := range changed_vars {
+					topic := ups.Name() + "/" + k
+					// upsd uses . and we use / - this isn't always the right way, but it often is, so *shrug*
+					topic = strings.Replace(topic, ".", "/", -1)
+					change_chan <- &MQTTUpdate{topic, v, old.Vars()[k]}
+				}
+			}
+		} else {
+			for k, v := range ups.Vars() {
+				topic := ups.Name() + "/" + k
+				// upsd uses . and we use / - this isn't always the right way, but it often is, so *shrug*
+				topic = strings.Replace(topic, ".", "/", -1)
+				change_chan <- &MQTTUpdate{topic, v, ""}
+			}
+		}
+		last_ups[ups.Name()] = ups
+	}
+}
+
+func (c *mqttClient) ConsumeChannelUpdates(change_chan chan *MQTTUpdate, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		update := <-change_chan
+		old := update.old_content
+		if old == "" {
+			old = "[null]"
+		}
+		log.Printf("MQTT Change: [%v]\t%v -> %v ", c.GetTopicBase()+update.topic, old, update.content)
+		c.PublishMessage(update)
+	}
 }
